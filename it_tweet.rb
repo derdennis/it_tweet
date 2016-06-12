26e66da978270225847ad5c0f856edee0a6ba407
Dennis Wegner <dennis@instant-thinking.de>
2016-06-12 08:56:14 +0200

    Updated to Ruby 1.9 because of outdated SSL cert lib, fixed the twitter stuff for the new gem.

--- it_tweet.rb
+++ it_tweet.rb
@@ -2,7 +2,7 @@
 
 # This script takes information from an RSS feed and posts it as updates to
 # Twitter. If you have a website or blog, you can just run this script every
-# time you make a new post and it'll automatically tweet about it. 
+# time you make a new post and it'll automatically tweet about it.
 #
 # This script is heavily based on the twitterscript2 by Michael Morin which can
 # be found at:
@@ -10,7 +10,7 @@
 # http://ruby.about.com/od/networking/qt/twitterscript2.htm
 #
 # The main configuration options are expected in config.yml in the same
-# directory as this script. 
+# directory as this script.
 
 # Require some gems
 require 'rubygems'
@@ -25,19 +25,17 @@ require 'pony'
 
 # Load config from yaml-file, find it in the @script_dir
 CONFIG = YAML.load_file("#{@script_dir}/config.yml") unless defined? CONFIG
-
 # Feed is here
 feed = CONFIG['feed']
 # Cache file for the last checked time is here
 tweet_times_file = @script_dir + "/" + CONFIG['cache_file']
-
 # Configure my OAuth for Twitter authentification, the actual keys live in
 # config.yml
-Twitter.configure do |config|
-  config.consumer_key = CONFIG['consumer_key']
-  config.consumer_secret = CONFIG['consumer_secret']
-  config.oauth_token = CONFIG['oauth_token']
-  config.oauth_token_secret = CONFIG['oauth_token_secret'] 
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key         = CONFIG['consumer_key']
+  config.consumer_secret      = CONFIG['consumer_secret']
+  config.access_token         = CONFIG['oauth_token']
+  config.access_token_secret  = CONFIG['oauth_token_secret']
 end
 
 # If the tweet_times file exists, load the content from the yaml into a hash
@@ -49,8 +47,7 @@ tweet_times = if File.exists?(tweet_times_file)
             end
 
 # Fetch and parse the feed with SimpleRSS
-rss = SimpleRSS.parse open(feed) 
-
+rss = SimpleRSS.parse open(feed)
 # Tell me you're actually doing something...
 print "Checking ", feed,  " for tweetable posts...", "\n"
 
@@ -58,13 +55,15 @@ print "Checking ", feed,  " for tweetable posts...", "\n"
 rss.entries.reverse.each_with_index do|i,idx|
     #If the publication date is later than the date in our hash, put it's
     #values into the text for a new tweet.
+p i.updated
+p tweet_times[feed]
     if i.updated > tweet_times[feed]
         text = "New Blogpost: #{i.title} #{i.link}"
         # Output the text
         puts text
         puts "=" * 50
         # Tweet the text
-        Twitter.update text
+        client.update(text)
 
         # Notify me via mail that the post was tweeted
         message = "Hi #{CONFIG['recipient_name']}, \n\n I just tweeted the following tweet: \n\n" + text.to_s + ". \n\n Best, \n\n #{CONFIG['from_name']}"
@@ -83,8 +82,8 @@ rss.entries.reverse.each_with_index do|i,idx|
             :enable_starttls_auto => true,
             :user_name            => CONFIG['smtp_user'],
             :password             => CONFIG['smtp_password'],
-            :authentication       => :plain, 
-            :domain               => "localhost.localdomain" 
+            :authentication       => :plain,
+            :domain               => "localhost.localdomain"
         } # End pony via options
         }) # End Pony.mail
 
@@ -94,7 +93,7 @@ rss.entries.reverse.each_with_index do|i,idx|
         File.open( tweet_times_file, 'w' ) do|f|
             f.write YAML.dump(tweet_times)
         end
-        
+
         # Sleep for 60 seconds
         sleep 60
     end # End if i.updated
